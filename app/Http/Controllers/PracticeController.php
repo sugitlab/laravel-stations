06f158dd8901921f6c2cc6fc76c3744706b9e316
sugitlab <sgmt.snj@gmail.com>
2022-09-11 21:43:03 +0900

    station4

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Practice;
+
 class PracticeController extends Controller
 {
+  public function getPractice()
+  {
+    $practice = Practice::all();
+    return response()->json($practice);
+  }
   public function sample()
   {
     return view('practice');
   }
   public function sample2()
   {
-    $test = 'Practice2';
+    $test = 'practice2';
     return view('practice2', ['testParam' => $test]);
   }
   public function sample3()
